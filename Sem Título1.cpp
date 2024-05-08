2d14ebc566972f51142f4449f263ca9223b93978
gabrielwsb <gabrielwsb@me.com>
2024-05-08 20:22:28 -0300

    Add files via upload

--- Sem Título1.cpp
+++ Sem Título1.cpp
@@ -132,6 +132,7 @@ int main (){
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do Sistema\n\n");
 		printf("Opção: "); //fim do menu
 	
 		scanf ("%d" , &opcao); //armazenando a escolha do usuário
@@ -152,6 +153,10 @@ int main (){
 			deletar();
 			break;
 			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;
 		
 			default:
 			printf("Essa opção não está disponível!\n");
